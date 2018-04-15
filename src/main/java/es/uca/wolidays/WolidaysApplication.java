4bcb44394894180790b03696604326e00e6f4ef3
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2018-04-15 12:53:10 +0200

    Creada la estructura del Proyecto.

---
+++ src/main/java/es/uca/wolidays/WolidaysApplication.java
@@ -0,0 +1,12 @@
+package es.uca.wolidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WolidaysApplication.class, args);
+	}
+}
