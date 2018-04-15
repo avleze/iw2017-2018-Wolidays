5fdbfa9246ceb9365ca35973cfd41a0e78578181
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2018-04-15 20:49:57 +0200

    Añadidos repositorios y GitIgnore.

---
+++ src/main/java/es/uca/wolidays/backend/repositories/RolRepository.java
@@ -0,0 +1,9 @@
+package es.uca.wolidays.backend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import es.uca.wolidays.backend.entities.Rol;
+
+public interface RolRepository extends CrudRepository<Rol, Integer>{
+
+}
---
+++ src/main/java/es/uca/wolidays/backend/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package es.uca.wolidays.backend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import es.uca.wolidays.backend.entities.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+
+}
