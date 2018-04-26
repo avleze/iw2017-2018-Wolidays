aa7e9fed12a82ca65b1489f040e600995feafc1c
Juan Antonio Rodicio Lopez <juanantonio.rodiciolopez@alum.uca.es>
2018-04-26 16:56:06 +0200

    Añadidos nuevos filtros para la búsqueda de apartamentos.

--- src/main/java/es/uca/wolidays/backend/repositories/ApartamentoRepository.java
+++ src/main/java/es/uca/wolidays/backend/repositories/ApartamentoRepository.java
@@ -1,7 +1,9 @@
 package es.uca.wolidays.backend.repositories;
 
+import java.sql.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import es.uca.wolidays.backend.entities.Apartamento;
@@ -12,4 +14,8 @@ public interface ApartamentoRepository extends CrudRepository<Apartamento, Integ
 	List<Apartamento> findByNumDormitorios(Integer numDormitorios);
 	List<Apartamento> findByNumCamas(Integer numCamas);
 	List<Apartamento> findByPrecioEstandarBetween(Double min, Double max);
+	@Query("SELECT apt FROM Apartamento apt WHERE apt.ubicacion = ?1 AND apt.precioEstandar BETWEEN ?2 AND ?3")
+	List<Apartamento> filterByUbicacionAndPrecio(String ubicacion, Double min, Double max);
+	@Query("SELECT apt FROM Apartamento apt, Reserva res WHERE apt.id = res.apartamento AND apt.ubicacion = ?1 AND res.fechaInicio < ?2 AND res.fechaFin > ?3")
+	List<Apartamento> filterByUbicacionAndFecha(String ubicacion, Date fechainicio, Date fechafin);
 }
