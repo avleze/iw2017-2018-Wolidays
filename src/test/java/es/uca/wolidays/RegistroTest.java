92bde64c32448558d276d1d922e7df5a71633704
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2018-05-14 11:27:41 +0200

    Añadidos test de selenium.

---
+++ src/test/java/es/uca/wolidays/RegistroTest.java
@@ -0,0 +1,119 @@
+package es.uca.wolidays;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+
+public class RegistroTest extends TestBase implements SauceOnDemandSessionIdProvider {
+	@Autowired
+	private Environment environment;
+
+	private static final String HOST_URL = "http://ec2-18-236-104-144.us-west-2.compute.amazonaws.com:";
+	private static final String XPATH_NAV_BTN_PERFIL = "//*[@id=\"nav_btn_perfil\"]";
+	private static final String XPATH_NAV_BTN_INICIOSESION = "//*[@id=\"nav_btn_iniciosesion\"]";
+	private static final String XPATH_BOTON_INICIOSESION = "//*[@id=\"form_btn_login\"]";
+	private static final String XPATH_BOTON_REGISTRARSE_FORMULARIO = "//*[@id=\"form_btn_registrate\"]";
+	private static final String XPATH_TEXTBOX_CONFIRMACIONCONTRASENA = "//*[@id=\"form_confirmpassword\"]";
+	private static final String XPATH_TEXTBOX_CONTRASENA = "//*[@id=\"form_password\"]";
+	private static final String XPATH_TEXTBOX_USERNAME = "//*[@id=\"form_username\"]";
+	private static final String XPATH_TEXTBOX_CORREO = "//*[@id=\"form_correo\"]";
+	private static final String XPATH_TEXTBOX_NOMBRE = "//*[@id=\"form_nombre\"]";
+	private static final String XPATH_TEXTBOX_APELLIDOS = "//*[@id=\"form_apellidos\"]";
+
+	private static final String XPATH_NAV_BTN_REGISTRARSE = "//*[@id=\"nav_btn_registrarse\"]";
+
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		ChromeOptions caps = new ChromeOptions();
+		caps.setCapability("platform", "Windows 10");
+		caps.setCapability("version", "latest");
+		this.driver = new RemoteWebDriver(new URL(URL), caps);
+		this.sessionId = (((RemoteWebDriver) driver).getSessionId()).toString();
+		this.wait = new WebDriverWait(driver, 100);
+	}
+
+	@Ignore
+	@Test
+	public void registrarse() {
+		driver.get(getHostUrl());
+		WebElement botonRegistrarse = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_NAV_BTN_REGISTRARSE)));
+		botonRegistrarse.click();
+
+		WebElement elementoFormulario = null;
+
+		elementoFormulario = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_NOMBRE)));
+		elementoFormulario.sendKeys("ANTONIO");
+		elementoFormulario = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_APELLIDOS)));
+		elementoFormulario.sendKeys("VÉLEZ ESTÉVEZ");
+		elementoFormulario = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_CORREO)));
+		elementoFormulario.sendKeys("antonio.velezestevez@alum.uca.es");
+		elementoFormulario = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_USERNAME)));
+		elementoFormulario.sendKeys("pruebausername");
+		elementoFormulario = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_CONTRASENA)));
+		elementoFormulario.sendKeys("pruebacontraseña");
+		elementoFormulario = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_CONFIRMACIONCONTRASENA)));
+		elementoFormulario.sendKeys("pruebacontraseña");
+
+		elementoFormulario = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_BOTON_REGISTRARSE_FORMULARIO)));
+		elementoFormulario.click();
+
+		assertTrue(wait.until(ExpectedConditions.urlContains("login")));
+	}
+
+	@Test
+	public void iniciarSesion() {
+		driver.get(getHostUrl());
+		WebElement botonRegistrarse = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_NAV_BTN_INICIOSESION)));
+		botonRegistrarse.click();
+
+		WebElement elementoFormulario = null;
+
+		elementoFormulario = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_USERNAME)));
+		elementoFormulario.sendKeys("pruebausername");
+		elementoFormulario = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_TEXTBOX_CONTRASENA)));
+		elementoFormulario.sendKeys("pruebacontraseña");
+
+		elementoFormulario = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_BOTON_INICIOSESION)));
+		elementoFormulario.click();
+
+		WebElement navBtnPerfil = wait
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(XPATH_NAV_BTN_PERFIL)));
+		assertTrue(navBtnPerfil.isDisplayed());
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	private String getHostUrl() {
+		return HOST_URL + environment.getProperty("local.server.port");
+	}
+}
---
+++ src/test/java/es/uca/wolidays/TestBase.java
@@ -0,0 +1,35 @@
+package es.uca.wolidays;
+
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+import com.saucelabs.common.SauceOnDemandAuthentication;
+import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
+
+public class TestBase implements SauceOnDemandSessionIdProvider{
+
+	public static final String USERNAME = "avleze";
+	public static final String ACCESS_KEY = "5acfd4e5-3c09-400a-b8be-44e843fcd417";
+	public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	protected String sessionId;
+	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(USERNAME, ACCESS_KEY);
+	@Rule
+	public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+	@Rule
+	public TestName name = new TestName() {
+			public String getMethodName() {
+				return String.format("%s", super.getMethodName());
+			}
+		};
+
+	public TestBase() {
+		super();
+	}
+
+	@Override
+	public String getSessionId() {
+		return sessionId;
+	}
+
+}
