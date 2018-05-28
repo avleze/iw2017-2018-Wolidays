ead031d9f8f76e4ab995fda50ad118ffb6e6957f
juanrodicio <juanantonio.rodiciolopez@alum.uca.es>
2018-05-29 00:24:43 +0200

    Creado método para obtener el pct de beneficio definido por la empresa

--- src/main/java/es/uca/wolidays/backend/services/TransaccionService.java
+++ src/main/java/es/uca/wolidays/backend/services/TransaccionService.java
@@ -1,7 +1,11 @@
 package es.uca.wolidays.backend.services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 
@@ -36,12 +40,28 @@ public class TransaccionService {
 				.getResultList();
 	}
 	
+	public Float obtenerPctBeneficioActual() {
+    	
+		String filename = "src/main/resources/beneficio";
+    	try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+    		
+    		Optional<String> beneficioStr = stream.findFirst();
+        	
+    		if(beneficioStr.isPresent())
+    			return Float.parseFloat(beneficioStr.get());
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	return null;
+	}
+	
 	public TransaccionPenalizacion guardar(TransaccionPenalizacion tP) {
 		return repoTranPenalizacion.save(tP);
 	}
 	
 	public TransaccionReserva guardar(TransaccionReserva tR) {
-		return repoTranReserva.save(tR);
+		return repoTranReserva.save(tR); 
 	}
 	
 	public void eliminarTransaccionPenalizacion(TransaccionPenalizacion tP) {
