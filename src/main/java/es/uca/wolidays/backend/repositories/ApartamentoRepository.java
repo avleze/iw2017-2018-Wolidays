4a6a1a7a802be2509dfd880cc527eadacb1a6093
juanrodicio <juanantonio.rodiciolopez@alum.uca.es>
2018-05-29 16:45:34 +0200

    Corrigiendo query para el filtro por fechas

--- src/main/java/es/uca/wolidays/backend/repositories/ApartamentoRepository.java
+++ src/main/java/es/uca/wolidays/backend/repositories/ApartamentoRepository.java
@@ -23,7 +23,7 @@ public interface ApartamentoRepository extends CrudRepository<Apartamento, Integ
 	List<Apartamento> findByPrecioEstandarBetween(Double min, Double max);
 	@Query("SELECT apt FROM Apartamento apt WHERE apt.ubicacion = ?1 AND apt.precioEstandar BETWEEN ?2 AND ?3")
 	List<Apartamento> filterByUbicacionAndPrecio(String ubicacion, Double min, Double max);
-	@Query("SELECT apt FROM Apartamento apt, Reserva res WHERE (apt.id = res.apartamento AND apt.ubicacion = ?1 AND res.fechaInicio < ?2 AND res.fechaFin > ?3) OR apt.id NOT IN (SELECT res.apartamento FROM Reserva res)")
+	@Query("SELECT apt FROM Apartamento apt, Reserva res WHERE (apt.id = res.apartamento AND apt.ubicacion = ?1 AND res.fechaInicio < ?2 AND res.fechaFin > ?3 AND res.estado != 'Validada') OR apt.id NOT IN (SELECT res.apartamento FROM Reserva res)")
 	List<Apartamento> filterByUbicacionAndFecha(String ubicacion, LocalDate fechainicio, LocalDate fechafin);
 	
 	@EntityGraph(value = "Apartamento.apartamentoConOfertas", type = EntityGraphType.LOAD)
